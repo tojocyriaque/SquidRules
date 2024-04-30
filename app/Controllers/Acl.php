caada894d65b65a83a36de0a0c838855aab55ff1
tojocyriaque <ramarosoncyriaque@gmail.com>
2024-04-30 18:28:04 +0300

    controlleur pour les configurations acl

---
+++ app/Controllers/Acl.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controllers;
+
+class Acl extends BaseController{
+  
+  public function index(): string{
+    return view("acl");
+  }
+  
+  public function modify(){
+
+  }
+
+  public function add(){
+
+  }
+
+}
+
+?>
+
+
