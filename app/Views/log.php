7dcea39967ebc4e188bfa6336a01766121a5ee5b
NirintsoaGRT <148960397+NirintsoaGRT@users.noreply.github.com>
2024-05-02 14:52:06 +0300

    Update log.php
    log efa mitambatra

--- app/Views/log.php
+++ app/Views/log.php
@@ -5,7 +5,7 @@
   <META name="viewport" content="width=device-width, initial-scale=1.0">
   <TITLE>proxy log</TITLE>
   <LINK href="<?= base_url('bootstrap-5.3.3-dist/css/bootstrap.min.css') ?>" rel="stylesheet">
-  <SCRIPT src="<?= base_url('bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js') ?>"></SCRIPT>
+  <SCRIPT src="<?=base_url('bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js')?>"></SCRIPT>
 </HEAD>
 <BODY class="bg-dark">
 <NAV class="navbar navbar-expand-xl navbar-dark bg-dark text-white">
@@ -13,7 +13,7 @@
 		<DIV class="col-md-6">
 			<UL class="navbar-nav">
 				<LI class="nav-item">
-					<A class="nav-link active" href="#">Dashboard</A>
+					<A class="nav-link active" href="http://www.devoir.com/index.php/acceuille">Dashboard</A>
 				</LI>
 				<LI class="nav-item">
 					<A class="nav-link" href="#">Proxy</A>
@@ -21,7 +21,7 @@
 			</UL>
 		</DIV>
 		<DIV class="col-md-6 d-flex justify-content-end">
-			<FORM class="d-flex" action="<?= base_url('index.php/listlog') ?>"  method=get>
+			<FORM class="d-flex" action="http://www.devoir.com/index.php/LogProxy/list"  method=get>
 				<INPUT name="datelog" class="form-control form-control-sm me-2" type="date" id="datePicker" value="<?= $date ?>">
 		        <INPUT name="search" class="form-control form-control-sm me-2" type="search" placeholder="Ip address" value="<?= $search ?>">
 		        <INPUT class="btn btn-sm btn-primary" type="submit" value="Search">
@@ -70,7 +70,7 @@
 			</UL>
 		</DIV>
 		<DIV class="col-md-6 d-flex justify-content-end">
-			<FORM class="d-flex" action="<?= base_url('index.php/delete') ?>"  method=get>
+			<FORM class="d-flex" action="http://www.devoir.com/index.php/LogProxy/list"  method=get>
 				<INPUT name="datedel" class="form-control form-control-sm me-2" type="date" id="datePicker" value="<?= $date ?>">
 		        <INPUT class="btn btn-sm btn-danger" type="submit" value="Delete">
 		    </FORM>
