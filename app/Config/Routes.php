6d6461982e18562902f28f658dce3af1da359c56
Nahlucky <124454587+Nahlucky@users.noreply.github.com>
2024-05-02 08:53:51 +0200

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,9 +1,12 @@
 <?php
 
 use CodeIgniter\Router\RouteCollection;
+use App\Controllers\LogProxy;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
 $routes->setAutoRoute(true);
+$routes->get('listlog', [LogProxy::class, 'list']);
+$routes->get('delete', [LogProxy::class, 'delete']);
