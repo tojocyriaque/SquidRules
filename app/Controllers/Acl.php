2bf1f625ea8cd372a8553e2e17dd71e95d49f323
tojocyriaque <ramarosoncyriaque@gmail.com>
2024-05-08 11:21:20 +0300

    Mise à jour de quelques méthodes

--- app/Controllers/Acl.php
+++ app/Controllers/Acl.php
@@ -11,6 +11,11 @@ include "acl/acl_access_get_name.php";
 include "acl/acl_access_get_active.php";
 include "acl/acl_access_get_all_declaration.php";
 
+include "acl/acl_access_add_name.php";
+include "acl/ajout.php";
+include "acl/modify.php";
+include "acl/delete.php";
+include "acl/acl_access_save.php";
 
 class Acl extends BaseController
 {
@@ -20,11 +25,58 @@ class Acl extends BaseController
 
     public function datas(): void{}
 
-    public function add(): void{}
+    public function ajout(): void{
+      $postdata = file_get_contents("php://input");
+      if(isset($postdata)&& !empty($postdata)){
+          $data = json_decode($postdata,true);
+          add($data);
+          $GLOBAL["value"]=return_data();
+          
+          $this->response->setJSON($GLOBAL["value"]);
+          echo $this->response->getJSON();
+      }
+      else{
+          http_response_code(400);
+          $this->response->setJSON(["Error" => "No data provided!!"]);
+          echo $this->response->getJSON();
+      }
+    }
 
-    public function modify(): void{}
+    public function modify(): void{
+      $postdata = file_get_contents("php://input");
+      if(isset($postdata) && !empty($postdata)){
+          $data=json_decode($postdata,true);
+          modify($data);
+          $GLOBAL["value"]=return_data();
+          $this->response->setJSON($GLOBAL["value"]);
+          echo $this->response->getJSON();
+          exit(0);
+      }else{
+          http_response_code(400);
+          $error=["Error" => "No data received!"];
+          $this->response->setJSON([$error]);
+          echo $this->response->getJSON();
+          exit(0);
+      }
+    }
 
-    public function delete(): void{}
+    public function delete(): void{
+      $data = file_get_contents("php://input");
+      $GLOBAL['value']=array();
+      if(isset($data) && !empty($data)){
+          $postdata = json_decode($data, true);
+          suppr_line($postdata["lineD"]);
+          put_datas();
+  
+          $GLOBAL["value"]=return_data();
+          $this->response->setJSON($GLOBAL["value"]);
+          echo $this->response->getJSON();
+      } else {
+          http_response_code(400);
+          $this->response->setJSON(["Error" => "No data received"]);
+          echo $this->response->getJSON();
+      }
+    }
 
     public function history(): void{
       $postdata=file_get_contents("php://input");
@@ -41,6 +93,19 @@ class Acl extends BaseController
       }
 
       echo $this->response->getJSON();
+    }
+
+    public function acl_access_save(): void{
+      $data = json_decode(file_get_contents('php://input'), true);
+      
+      $access = $data['access1'];
+      $name = $data['name1'];
+      $check = $data['check1'];
+      
+      var_dump($data);
+
+      $rules = ["access" => $access, "name" => $name];
+      save_autorise($check, $rules);
     }
 
     // finished
@@ -65,11 +130,12 @@ class Acl extends BaseController
       echo $this->response->getJSON();
     }
 
-    public function acl_access_add_name(){}
-
+    public function acl_access_add_name(): void{
+      acl_access_add_name();
+    }
     public function give_type(){}
 
-    public function acl_change_type(){}
+    public function change_type(){}
 
     public function restore(){}
 
